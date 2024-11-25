d0769636a7db6bdd5b7d0271e0cc7f3bd2a8983d
Cristhian Jesus <cristhianhuamayauris@gmail.com>
2024-11-25 11:55:08 -0500

    visualización

--- src/app/task/features/tasklist/tasklist.component.ts
+++ src/app/task/features/tasklist/tasklist.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { PagComponent } from '../../ui/pag/pag.component';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-tasklist',
   standalone: true,
-  imports: [PagComponent],
+  imports: [PagComponent, RouterLink],
   templateUrl: './tasklist.component.html',
   styleUrl: './tasklist.component.css'
 })
